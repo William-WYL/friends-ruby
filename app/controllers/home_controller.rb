737988caedd36494699f66f7691c6bafb5fd6ace
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-07-20 14:48:52 -0500

    Create a scaffold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # This action can be used to render the home page.
+  end
+
+  def about
+    # This action can be used to render an 'About Us' page.
   end
 end
